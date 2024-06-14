34805c48c3ccb808dbc68430d2574c1db7dfdebf
forellko <dragonforce97q@gmail.com>
2024-06-15 02:14:10 +0300

    split config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,17 @@
-import path from 'path';
-import HTMLWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
-import { buildPlugins } from './config/build/buildPlugins';
-import { buildLoaders } from './config/build/buildLoader';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths } from './config/build/types/config';
+import path from 'path';
 
-const config: webpack.Configuration = {
-  mode: 'production',
-  entry: { index: path.resolve(__dirname, 'src', 'index.ts') },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  plugins: buildPlugins(),
-  module: {
-    rules: buildLoaders(),
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  build: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html'),
 };
+
+const config: webpack.Configuration = buildWebpackConfig({
+  mode: 'development',
+  paths: paths,
+});
 
 export default config;
