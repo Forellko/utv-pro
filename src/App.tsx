54e6b726922b88ca7dbf7239c6df8a1e341962bc
forellko <dragonforce97q@gmail.com>
2024-06-24 22:27:22 +0300

    fix lazy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import AboutPage from './pages/AboutPage/AboutPage';
 import MainPage from './pages/MainPage/MainPage';
 import { Link } from 'react-router-dom';
+import AboutPageLazy from './pages/AboutPage/AboutPageLazy';
+import MainPageLazy from './pages/MainPage/MainPageLazy';
 
 const App = () => {
   return (
@@ -9,8 +11,8 @@ const App = () => {
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Routes>
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPageLazy />} />
+        <Route path="/" element={<MainPageLazy />} />
       </Routes>
     </div>
   );
