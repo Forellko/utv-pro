637f07360af0f2e8844439a8786ddf3dc1baf01e
forellko <dragonforce97q@gmail.com>
2024-06-24 21:53:52 +0300

    Create App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/about" />
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { Counter } from './components/Counter';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
