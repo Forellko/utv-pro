44317cfeaa784a988adcb38e35934f3ac08b170a
forellko <dragonforce97q@gmail.com>
2024-06-24 22:03:05 +0300

    Create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app">
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
       <Routes>
-        <Route path="/about" />
-        <Route path="/" />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </div>
   );
